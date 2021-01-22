d910b52547253c533f53777a6e310eb5d977daa0
PPF\yagi.hiroki <yagi.hiroki@ppfpart.co.jp>
2021-01-22 11:48:29 +0900

    学習

---
+++ Sample/Assets/Test.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player
+{
+    private int hp = 100;
+    private int power = 50;
+
+    public void attack()
+    {
+        Debug.Log(this.power + "のダメージを与えた");
+    }
+
+    public void Damage(int damage)
+    {
+        this.hp -= damage;
+        Debug.Log(damage + "のダメージを受けた");
+    }
+}
+
+
+public class Test : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector2 playerPos = new Vector2(3.0f, 4.0f);
+        playerPos.x += 8.0f;
+        playerPos.y += 5.0f;
+        Debug.Log(playerPos);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
